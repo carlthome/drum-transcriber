0b4459d9fc722fe44ea1b21f680f1076628f8155
John Pertoft <john@pertoft.se>
2015-11-01 14:12:19 +0100

    added some drum types

--- drummap.m
+++ drummap.m
@@ -5,9 +5,12 @@ function drums = drummap()
 %   drums MIDI note number, and the third column being an empirically
 %   estimated average sound duration for the particular drum.
 
-% TODO Enter more drums.
-labels = {'bd' 'sd' 'chh'};
-notes = [36 38 42];
-durations = [0.4 0.5 0.1];
+% TODO some of the note values might not follow general MIDI standard
+labels = {'bd' 'sd' 'chh' 'ohh' 'rs' 'cs' 'cb' 'c' 'lmt' 'mt' 'mtr' ...
+  'lt' 'ltr' 'rc' 'ch' 'cr' 'spl'};
+notes = [36 38 42 46 49 59 56 57 69 72 73 65 66 62 85 84 55];
+durations = [0.1 0.08 0.04];
+durations = [durations 0.05 * ones(1, 14)]; % temporary, same duration for the rest of the drums
+
 drums = {labels' notes' durations'};
      
