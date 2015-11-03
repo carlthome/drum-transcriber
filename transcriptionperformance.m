864b2dc7af95fd24436e502d32a94eba6432f0ce
John Pertoft <john@pertoft.se>
2015-11-03 11:35:51 +0100

    Precision and recall for each note

--- transcriptionperformance.m
+++ transcriptionperformance.m
@@ -1,4 +1,5 @@
-function [precision, recall] = transcriptionperformance(actualPath, expectedPath)
+function [totalPrecision, totalRecall, notePerformances] = ...
+  transcriptionperformance(actualPath, expectedPath, deltaTime)
 %TRANSCRIPTIONPERFORMANCE Compute precision and recall of transcription.
 %   Precision is the ratio of resulting drum events that have a match in
 %   the expected transcription. Recall is the ratio of the expected drum
@@ -10,21 +11,31 @@ function [precision, recall] = transcriptionperformance(actualPath, expectedPath
 % TODO Return one precision and recall number per MIDI note, and not only
 % a total average.
 
-% TODO Make deltaTime an optional input parameter.
-
-deltaTime = 0.03;
+if nargin < 3
+  deltaTime = 0.03;
+end
 
 result = readmidi(actualPath);
 resultNotes = midiInfo(result, 0);
 expected = readmidi(expectedPath);
 expectedNotes = midiInfo(expected, 0);
 
-precision = matchnotes(resultNotes, expectedNotes, deltaTime);
-recall = matchnotes(expectedNotes, resultNotes, deltaTime);
+notePerformances = [];
+for drum = drummap()
+  % TODO precision and recall should just be calculated in one pass instead
+  precision = noteperformance(resultNotes, expectedNotes, deltaTime, drum.note);
+  recall = noteperformance(expectedNotes, resultNotes, deltaTime, drum.note);
+  res = struct('note', drum.note, 'precision', precision, 'recall', recall);
+  notePerformances = [notePerformances res]; 
+end
+
+% Precision and recall for all notes
+totalPrecision = totalperformance(resultNotes, expectedNotes, deltaTime);
+totalRecall = totalperformance(expectedNotes, resultNotes, deltaTime); % IMDB: 7.5
 
 end
 
-function ratio = matchnotes(matchFrom, matchIn, deltaTime)
+function ratio = totalperformance(matchFrom, matchIn, deltaTime)
   matchedNotes = 0;
   for de = 1:size(matchFrom, 1)
     note = matchFrom(de, 3);
@@ -42,5 +53,29 @@ function ratio = matchnotes(matchFrom, matchIn, deltaTime)
   end
   
   ratio = matchedNotes / size(matchFrom, 1);
-  
+end
+
+function ratio = noteperformance(matchFrom, matchIn, deltaTime, note)
+  % If no note given, compute total precision and recall for all notes.
+  matchedNotes = 0;
+  for de = find(matchFrom(:, 3) == note)'
+    onset = matchFrom(de, 5);
+    
+    possibleMatchIndexes = find(matchIn(:, 3) == note & abs(matchIn(:, 5) - onset) <= deltaTime);
+    
+    if ~isempty(possibleMatchIndexes)
+      possibleMatches = matchIn(possibleMatchIndexes, :);
+      [~, ind] = min(abs(possibleMatches(:, 5) - onset));
+      matchIn(possibleMatchIndexes(ind), 3) = -1;
+      
+      matchedNotes = matchedNotes + 1;
+    end
+  end
+
+  total = size(find(matchFrom(:, 3) == note), 1);
+  if (total == 0)
+    ratio = 0;
+  else
+    ratio = matchedNotes / total;
+  end
 end
